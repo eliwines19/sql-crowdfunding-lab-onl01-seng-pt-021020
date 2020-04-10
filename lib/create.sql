cb3a5b2ad247c51a95a5ebefa4ed5cbf5530485b
Elijah wines <elijahwines84@yahoo.com>
2020-04-10 17:23:34 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,8 @@
 CREATE TABLE projects (
-  id INTEGER PRIMARY KEY, 
+  id INTEGER PRIMARY KEY,
   title TEXT,
-  category TEXT, 
-  funding_goal INTEGER, 
+  category TEXT,
+  funding_goal INTEGER,
   start_date INTEGER,
-  end_date INTEGER 
+  end_date INTEGER
 );
