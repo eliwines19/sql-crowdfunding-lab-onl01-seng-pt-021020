81764c531bde3a7f21cb83b1cb9b9ae19470ed23
Elijah wines <elijahwines84@yahoo.com>
2020-04-10 17:43:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,23 +10,23 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectNINE", "history", 5000, 090120, 090520);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectTEN", "criminal justice", 17000, 100120, 100520);
 
-INSERT INTO users (name, age) VALUES ("John", 27)
-INSERT INTO users (name, age) VALUES ("Jacob", 23)
-INSERT INTO users (name, age) VALUES ("Jingle", 25)
-INSERT INTO users (name, age) VALUES ("Heimer", 13)
-INSERT INTO users (name, age) VALUES ("Smith", 19)
-INSERT INTO users (name, age) VALUES ("His", 17)
-INSERT INTO users (name, age) VALUES ("Name", 19)
-INSERT INTO users (name, age) VALUES ("Is", 32)
-INSERT INTO users (name, age) VALUES ("My", 45)
-INSERT INTO users (name, age) VALUES ("Nombre", 50)
-INSERT INTO users (name, age) VALUES ("Too", 33)
-INSERT INTO users (name, age) VALUES ("Whenever", 12)
-INSERT INTO users (name, age) VALUES ("We", 25)
-INSERT INTO users (name, age) VALUES ("Go", 56)
-INSERT INTO users (name, age) VALUES ("Out", 66)
-INSERT INTO users (name, age) VALUES ("The", 77)
-INSERT INTO users (name, age) VALUES ("People", 39)
-INSERT INTO users (name, age) VALUES ("Always", 20)
-INSERT INTO users (name, age) VALUES ("Shout", 47)
-INSERT INTO users (name, age) VALUES ("Hisname", 90)
+INSERT INTO users (name, age) VALUES ("John", 27);
+INSERT INTO users (name, age) VALUES ("Jacob", 23);
+INSERT INTO users (name, age) VALUES ("Jingle", 25);
+INSERT INTO users (name, age) VALUES ("Heimer", 13);
+INSERT INTO users (name, age) VALUES ("Smith", 19);
+INSERT INTO users (name, age) VALUES ("His", 17);
+INSERT INTO users (name, age) VALUES ("Name", 19);
+INSERT INTO users (name, age) VALUES ("Is", 32);
+INSERT INTO users (name, age) VALUES ("My", 45);
+INSERT INTO users (name, age) VALUES ("Nombre", 50);
+INSERT INTO users (name, age) VALUES ("Too", 33);
+INSERT INTO users (name, age) VALUES ("Whenever", 12);
+INSERT INTO users (name, age) VALUES ("We", 25);
+INSERT INTO users (name, age) VALUES ("Go", 56);
+INSERT INTO users (name, age) VALUES ("Out", 66);
+INSERT INTO users (name, age) VALUES ("The", 77);
+INSERT INTO users (name, age) VALUES ("People", 39);
+INSERT INTO users (name, age) VALUES ("Always", 20);
+INSERT INTO users (name, age) VALUES ("Shout", 47);
+INSERT INTO users (name, age) VALUES ("Hisname", 90);
