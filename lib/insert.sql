89a6514c4a807c305c9f5eb773e11c3978b8d215
Elijah wines <elijahwines84@yahoo.com>
2020-04-10 17:37:43 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,12 @@
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectONE", "science", 10000, 010120, 01052020);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectONE", "music", 10000, 010120, 01052020);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectTWO", "math", 15000, 020120, 020520);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectTHREE", "art", 12000, 030120, 030520);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectFOUR", "social studies", 10000, 040120, 040520);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectFIVE", "Theater", 20000, 050120, 050520);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectSIX", "science", 18000, 060120, 060520);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectSEVEN", "sports", 13000, 070120, 070520);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectEIGHT", "science", 25000, 080120, 080520);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectEIGHT", "media", 25000, 080120, 080520);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectNINE", "history", 5000, 090120, 090520);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("ProjectTEN", "criminal justice", 17000, 100120, 100520);
 
